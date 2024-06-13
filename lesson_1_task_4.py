f17edbd4e78aa1174b89b6cfdab8a1f44027faf0
hikita2162 <hikita-14@yandex.ru>
2024-06-13 15:45:27 +0300

    работа1

---
+++ lesson_1_task_4.py
@@ -0,0 +1,3 @@
+last_name = input('Догадин')
+first_name = input('никита')
+print('Вас зовут: ' + first_name + last_name)
---
+++ lesson_1_task_6.py
@@ -0,0 +1,42 @@
+def print_a():
+    print("8", end="")
+
+
+def print_b():
+    print("8", end="")
+ 
+
+def print_c():
+    print("0", end="")
+
+
+def print_d():
+    print("0", end="")
+
+
+def print_i():
+    print("5", end="")
+
+
+def print_f():
+    print("5", end="")
+
+
+def print_g():
+    print("5", end="")
+
+def print_h():
+    print("3", end="")
+
+
+def print_y():
+    print("5", end="")
+
+
+def print_s():
+    print("3", end="")
+
+def print_x():
+    print("5", end="")
+
+print_a(), print_b(), print_c(), print_d(), print_i(), print_f(), print_g(), print_h(), print_y(), print_s(), print_x()
