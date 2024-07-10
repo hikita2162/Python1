e0f171fa9e66b6fadc865360272e09b1f164a242
hikita2162 <hikita-14@yandex.ru>
2024-07-10 16:52:35 +0300

    работа 3

--- lesson3/lesson_3_task_3.py
+++ lesson3/lesson_3_task_3.py
@@ -1,9 +1,9 @@
 from Address import Address
-from mailing import mailing
+from Mailing import Mailing
 
 to_address = Address("14145", "Москва","беговая","12","238")
 from_address = Address("10013", "Калуга","деревня хохловка","15","15")
-mailing = mailing(to_address, from_address, 500,"AKA477")
+Mailing = Mailing(to_address, from_address, 500,"AKA477")
 
 print(f"Отпровление 0{mailing.track} из {mailing.from_address.index}, {mailing.from_address.city},"
       f" {mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment}"
