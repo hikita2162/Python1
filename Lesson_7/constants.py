8cc7dd2d8a756b8b653f2f3afdc4da2fdf66cbfd
hikita2162 <hikita-14@yandex.ru>
2024-08-14 11:03:04 +0300

    Работа 7

---
+++ Lesson_7/constants.py
@@ -0,0 +1,5 @@
+Test_form_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.htm"
+
+Calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+
+Shop_URL = "https://www.saucedemo.com/"
